b5b7dac1c6d7898a91325656026dcbdd7d9ac12f
Muhiddin <103505764+Muhiddin12@users.noreply.github.com>
2023-04-07 18:55:51 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const containerEl = document.querySelector(".container");
+const leftEl = document.querySelector(".left");
+const rightEl = document.querySelector(".right");
+
+leftEl.addEventListener("mouseenter", ()=> {
+
+});
